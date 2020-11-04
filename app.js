12c342217c6b9d60763ec310fbd84bc4839b4e60
Amari James <mar.james1998@gmail.com>
2020-11-04 11:20:05 -0500

    working still

---
+++ app.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded',() => {
+    const grid = document.querySelector('.grid')
+    let tiles = Array.from(document.querySelectorAll('.grid div'))
+    const width = 10
+
+    console.log(tiles)
+
+
+})
